afb5cf7d03886a86dc488196560ceb3bc46040bf
Dmitriy Born <1111born1111@gmail.com>
2022-11-27 16:38:41 +0300

    Создание метода подсчета количесвта элементов в массиве меньше или раные 3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿Console.Write("Введите текст через запятую для проверки по длине элемента: ");
 string[] text = Console.ReadLine()!.Split(',');
 Console.WriteLine("Изначальный массив данных: ");
+
+
+int count = 0;
+
+int S = Quantity(text, count);
+string[] Mass = new string[S];
+
+//Метод подсчета количесвта элементов в массиве меньше или раные 3
+int Quantity(string[] massive, int num)
+{
+    for(int i = 0; i < massive.Length; i++)
+    {
+        if(massive[i].Length <= 3)
+        {
+            num++;
+        }
+    }
+    return num;
+}
